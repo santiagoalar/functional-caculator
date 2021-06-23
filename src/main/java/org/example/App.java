1f2d59a39f1ed227ac79c58ebd796450e33c91f4
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-23 00:00:28 -0500

    Refactoring divide function

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,28 +11,25 @@ public class App
         IntBinaryOperator sum = Integer::sum;
         IntBinaryOperator sub = (x,y)-> x-y;
 
+        IntBinaryOperator mul = getMulOperator(sum);
+        IntBinaryOperator division = getDivOperator(sub);
+
+        System.out.println(mul.applyAsInt(3,5));
+        System.out.println(division.applyAsInt(60,10));
+
+    }
+
+    private static IntBinaryOperator getDivOperator(IntBinaryOperator sub) {
+        IntBinaryOperator division = (x, y) -> (int) IntStream.iterate(x, e -> sub.applyAsInt(e, y))
+                .takeWhile(a -> a >= y).count();
+        return division;
+    }
+
+    private static IntBinaryOperator getMulOperator(IntBinaryOperator sum) {
         IntBinaryOperator mul = (x1,y1)-> IntStream.rangeClosed(0, y1)
                 .reduce((subtotal, element) -> sum.applyAsInt(subtotal, x1))
                 .getAsInt();
-
-        System.out.println(mul.applyAsInt(3,5));
-
-        /*IntBinaryOperator div = (x1,y1) -> IntStream.range(0, y1)
-                .reduce((subtotal, element) -> {
-                    System.out.println(element);
-                    return sub.applyAsInt(x1, y1);})
-                .getAsInt();*/
-
-        System.out.println(div(120, 10));
-
+        return mul;
     }
 
-    public static int div(int a, int b){
-        int times = 0;
-        while (a >= b){
-            a = a-b;
-            times++;
-        }
-        return times;
-    }
 }
